85507123a0a37e47dfc94abdfdd7de65a650f9d9
Mikhail Novichkin <mnovichkin@npoprogress.com>
2016-02-03 16:23:16 +0300

    Added extensions to allow more control over how endpoint values are generated

--- lib/grape-swagger/doc_methods.rb
+++ lib/grape-swagger/doc_methods.rb
@@ -547,6 +547,46 @@ module GrapeSwagger
             end
 
             operation[:nickname] = route.route_nickname if route.route_nickname
+
+            # Prepare data for `:consumes` and `:produces` keys customization
+            format = endpoint.namespace_inheritable(:format)
+            all_content_types = endpoint.namespace_stackable(:content_types).each_with_object({}) { |e, h| h.merge!(e) }
+
+            # Customize `:consumes` key
+            consumes_content_types = [all_content_types[format]] # only use one that current format parses
+            operation[:consumes] = consumes_content_types if route.route_method != 'GET' && consumes_content_types && !consumes_content_types.empty?
+
+            # Customize `:produces` key
+            route_produces = route.route_produces || endpoint.namespace_inheritable(:default_produces)
+            produces_content_types =
+              if route_produces == :take_consumes
+                consumes_content_types
+              elsif !route_produces.nil?
+                Array.wrap(route_produces).map { |e| all_content_types[e] || e }
+              end
+            operation[:produces] = produces_content_types if produces_content_types
+
+            # Allows to add common custom headers as parameters for a set of routes
+            # Passed with `namespace_stackable(:swagger_request_headers, value)` at some namespace
+            custom_request_headers = endpoint.namespace_stackable(:swagger_request_headers)
+            if custom_request_headers && !custom_request_headers.empty?
+              headers = custom_request_headers.each_with_object({}) { |e, h| h.merge!(e) }
+              additional_params = @@documentation_class.parse_header_params(
+                headers,
+                scope: i18n_scope,
+                key: i18n_key
+              )
+              operation[:parameters] += additional_params
+            end
+
+            # Passed with `namespace_stackable(:swagger_options, value)` at some namespace
+            global_options = endpoint.namespace_stackable(:swagger_options)
+            global_options.each { |e| operation.merge!(e) }
+
+            # Passed with `:swagger` key on `desc` call for route description
+            other_options = route.route_swagger || {}
+            operation.merge!(other_options)
+
             operation
           end.compact
           apis << {
